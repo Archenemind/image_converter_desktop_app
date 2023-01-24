7397b6c1f951ce9ed5852ef68e5b1ffb91abafdc
Carlos Garcia <qesloqpongo@gmail.com>
2023-01-23 21:31:05 -0500

    small fixes

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from directory_script import directory_script
 
-option = 'Y'
-while option == 'Y':
-    path = input("Writte the name of the file to convert: ")
-    option = input('Y/N\n')
-    
+path = input("Writte the name of the file to convert: ")
 
 directory_script(path)
 
