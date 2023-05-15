7eb028bbbca0c978cc6da884f1114731759994e5
Carlos Garcia <qesloqpongo@gmail.com>
2023-05-14 22:28:57 -0400

    Added display image functionality, flag icon and user input validation

--- qt_file.py
+++ qt_file.py
@@ -1,22 +1,46 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 from directory_script import directory_script
-import _pickle
+from PIL import Image
+# import _pickle
 class Ui_Import_data(object):
     def browse_data1(self):
         data_path=QtWidgets.QFileDialog.getExistingDirectory(None,'Select a folder',r"D:\Programming")
         
-        directory_script(data_path)
+        if data_path != '':
+        
+            directory_script(data_path)
+        
+    def show_image(self):
+        im=QtWidgets.QFileDialog.getOpenFileName(None,'Select a file',r"D:\Programming")
+        
+        image_path = str(im[0])
+        
+        if image_path.endswith (('.jpg','.png','.avif','.webp')):
+        
+            im = Image.open(rf'{image_path}')
+            im.show()
+            
+        else:
+            self.message = QMessageBox()
+            self.message.setIcon(QMessageBox.Icon.Warning)
+            self.message.setText('This is not an image')          
+            self.message.show()
+            
         # with open('data_path.pickle', 'wb') as handle:
         #     _pickle.dump(data_path,handle,protocol=_pickle.HIGHEST_PROTOCOL)
 
     def setupUi(self, Import_data):
+        Import_data.setWindowIcon(QtGui.QIcon('flagofCuba_6551.png'))
+        
+        
         Import_data.setObjectName("Import_data")
         Import_data.resize(704, 131)
         self.pushButton = QtWidgets.QPushButton(Import_data)
         self.pushButton.setGeometry(QtCore.QRect(600, 20, 81, 23))
         self.pushButton.setObjectName("pushButton")
         self.label = QtWidgets.QLabel(Import_data)
-        self.label.setGeometry(QtCore.QRect(10, 50, 47, 13))
+        self.label.setGeometry(QtCore.QRect(10, 50, 100, 16))
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(Import_data)
         self.label_2.setGeometry(QtCore.QRect(10, 20, 121, 16))
@@ -33,24 +57,22 @@ class Ui_Import_data(object):
         self.pushButton_3 = QtWidgets.QPushButton(Import_data)
         self.pushButton_3.setGeometry(QtCore.QRect(600, 100, 81, 23))
         self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(Import_data)
-        self.pushButton_4.setGeometry(QtCore.QRect(510, 100, 75, 23))
-        self.pushButton_4.setObjectName("pushButton_4")
+        
         self.retranslateUi(Import_data)
 
         self.pushButton.clicked.connect(self.browse_data1)
+        self.pushButton_2.clicked.connect(self.show_image)
         QtCore.QMetaObject.connectSlotsByName(Import_data)
 
     def retranslateUi(self, Import_data):
         _translate = QtCore.QCoreApplication.translate
-        Import_data.setWindowTitle(_translate("Import_data", "Import Data"))
+        Import_data.setWindowTitle(_translate("Import_data", "Image Converter"))
         self.pushButton.setText(_translate("Import_data", "Browse"))
-        self.label.setText(_translate("Import_data", "Data Path"))
-        self.label_2.setText(_translate("Import_data", "Ground Truth Data Path"))
-        self.pushButton_2.setText(_translate("Import_data", "Browse"))
-        self.pushButton_3.setText(_translate("Import_data", "Apply"))
-        self.pushButton_4.setText(_translate("Import_data", "OK"))
-
+        self.label.setText(_translate("Import_data", "Display Image"))
+        self.label_2.setText(_translate("Import_data", "Browse the folder"))
+        self.pushButton_2.setText(_translate("Import_data", "Show"))
+        self.pushButton_3.setText(_translate("Import_data", "OK"))
+        
 
 if __name__ == "__main__":
     import sys
